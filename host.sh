b669c3e23748ebea6a53612090055814cff4b20c
root <root@ip-100-0-0-239.ap-south-1.compute.internal>
2017-07-23 19:25:18 +0000

    added if else conditon

--- host.sh
+++ host.sh
@@ -1,18 +1,18 @@
-arg=$1 # take argument from the command line and store in arg vraible
-if [ -z "$arg" ]
+arg=$1 # take argument from the command line and store in arg varible
+if [ -z "$arg" ] # cheak for the command line arugumet 
 then
-	echo	"pass agrument throug command line";
+	echo	"pass agrument throgh command line";
 
 else
-	for i in $(echo $arg | sed "s/,/ /g") #seprate the host name by the delimeter using comma and pust the value in variable i 
+	for i in $(echo $arg | sed "s/,/ /g") #replace the comma with space  and pust the value in the variable i 
 	do
    		 echo "$i" >>/tmp/host.txt #append the host name in host.text file
 	done
 	echo "enter the command to execute" 
 	read command # take the input from the user
-	for host in $(cat /tmp/host.txt); #read each hostname for the file host.txx  and give  the vaule  host variable
+	for host in $(cat /tmp/host.txt); #read each hostname for the file host.txx  and give  the vaulei to  host variable
 	do
-        	ssh  "$host" "$command" >>"/tmp/output.$host"; # every time it will loging to remote host  and execute the command that we had taken input and pust the output of the command to output.host file
+        	ssh  "$host" "$command" >>"/tmp/output.$host"; # every time it will loging to remote host  and execute the command that we had taken input and push the output of the command to output.host file
         	cat "/tmp/output.$host" # display the output
         	rm -rf /tmp/host.txt "/tmp/output.$host" # remove the host.txt anf output file form /tmp directory
 	done
